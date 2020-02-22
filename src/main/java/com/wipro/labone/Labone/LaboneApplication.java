ec69d2a36ff6af709ff0a25db2b1feac7aae5e5b
VenkatM18 <venkata83.maddu@gmail.com>
2020-02-22 13:03:45 +0530

    committed the code for Initial version for Labone at Wipro..

---
+++ src/main/java/com/wipro/labone/Labone/LaboneApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.labone.Labone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wipro/labone/Labone/LaboneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.labone.Labone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
