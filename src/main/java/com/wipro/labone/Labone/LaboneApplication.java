b45d541609f91e6e978e6f8a276e85624ec81d1b
VenkatM18 <venkata83.maddu@gmail.com>
2020-03-11 15:20:02 +0530

    11-03-2020

--- src/main/java/com/wipro/labone/Labone/LaboneApplication.java
+++ src/main/java/com/wipro/labone/Labone/LaboneApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LaboneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaboneApplication.class, args);
+        System.out.println("Inside LaboneApplication");
+	    SpringApplication.run(LaboneApplication.class, args);
+
 	}
 
 }
